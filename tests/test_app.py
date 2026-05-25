622da64fe8de93994549f1dd94a1629390c1ef75
kikitheguillotine <175551755+kikitheguillotine@users.noreply.github.com>
2026-05-25 14:51:11 -0300

    test: initial experiments

---
+++ tests/test_app.py
@@ -0,0 +1,21 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fastapi_study_project.app import app
+
+
+def test_root_must_return_hello_world():
+    """
+    Triple A test
+    A: Arrange
+    A: Act - executes SUT
+    A: Assert - asserts that x is x
+    """
+    # arrange
+    client = TestClient(app)
+    # act
+    response = client.get('/')
+    # assert
+    assert response.json() == {'message': 'Hello world'}
+    assert response.status_code == HTTPStatus.OK
