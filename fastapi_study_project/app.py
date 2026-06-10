ad6b66e9e7ca049583a0f2f1144e60667944a8fb
Patricia Kapla <patriciakapla@gmail.com>
2026-06-10 20:02:24 -0300

    feat: implement create, read, and update routes

--- fastapi_study_project/app.py
+++ fastapi_study_project/app.py
@@ -1,11 +1,18 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 
-from fastapi_study_project.schemas import Message
+from fastapi_study_project.schemas import (
+    Message,
+    UserDB,
+    UserList,
+    UserPublic,
+    UserSchema,
+)
 
 app = FastAPI()
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
@@ -13,7 +20,36 @@ def read_root():
     return {'message': 'Hello world'}
 
 
-@app.get('/vamps', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+@app.get('/vamps/', status_code=HTTPStatus.OK, response_class=HTMLResponse)
 def read_vamps():
     return '<h1>Vamps:</h1>\
         <p>Angel, Spike, Drusilla</p>'
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,34 +1,87 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fastapi_study_project.app import app
-
-
-def test_root_must_return_hello_world():
-    """
-    Triple A testD
-    A: Arrange
-    A: Act - executes SUT
-    A: Assert - asserts that x is x
-    """
-    # arrange
-    client = TestClient(app)
-    # act
+def test_root_must_return_hello_world(client):
     response = client.get('/')
-    # assert
     assert response.json() == {'message': 'Hello world'}
     assert response.status_code == HTTPStatus.OK
 
 
-def test_vamps_must_return_html_with_vamps_names():
-    client = TestClient(app)
+def test_vamps_must_return_html_with_vamps_names(client):
     response = client.get('/vamps')
-    assert response.text == '<h1>Vamps:</h1>\
+    assert (
+        response.text
+        == '<h1>Vamps:</h1>\
         <p>Angel, Spike, Drusilla</p>'
+    )
 
 
-def test_vamps_status_code_OK():
-    client = TestClient(app)
+def test_vamps_status_code_ok(client):
     response = client.get('/vamps')
     assert response.status_code == HTTPStatus.OK
+
+
+def test_create_user(client):
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret123',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'alice',
+        'email': 'alice@example.com',
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'alice',
+                'email': 'alice@example.com',
+            }
+        ]
+    }
+
+
+def test_update_client(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mistery',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_update_client_exception(client):
+    response = client.put(
+        '/users/2',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mistery',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
